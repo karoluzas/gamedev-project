58b7c9b7ecc84afaa39cf50888d3ecfbe189ae86
Brazauskas Deividas <deividas.brazauskas@mif.stud.vu.lt>
2022-10-28 12:50:20 +0300

    simple-belphegor-charge-ai

--- Assets/BelphegorAI.cs
+++ Assets/BelphegorAI.cs
@@ -3,49 +3,48 @@ using System.Collections.Generic;
 using UnityEngine;
 using Pathfinding;
 
-public class SathanasAI : MonoBehaviour
+public class BelphegorAI : MonoBehaviour
 {
 
     public Transform target;
-    public GameObject bulletPrefab;
 
     public float acceleration = 1f;
     public float maxSpeed = 1f;
-    
     public float nextWaypointDistance = 3f;
-    public float detectionRange = 1f;
-    
-    public float bulletForce = 4f;
-    public float rateOfFire = 1f;
 
-    float timeLeft = 0f;
-    bool canFire;
+    public float detectionRange = 0.8f;
+
+    public float waitTime = 0.5f;
+    public float chargeSpeed = 3f;
+
+    float timeLeft;
 
     Path path;
     int currentWaypoint = 0;
     bool reachedEndOfPath = false;
 
-     
+    bool canCharge = false;
+
     Seeker seeker;
     Rigidbody2D rb;
-    GameObject aim;
 
     // Start is called before the first frame update
     void Start()
     {
         seeker = GetComponent<Seeker>();
         rb = GetComponent<Rigidbody2D>();
-        aim = this.transform.Find("Aim").gameObject;
+
+        timeLeft = waitTime;
 
         InvokeRepeating("UpdatePath", 0f, .5f);
         seeker.StartPath(rb.position, target.position, OnPathComplete);
+
     }
 
     void UpdatePath()
     {
-        if (seeker.IsDone() && !InShootingRange())
+        if (seeker.IsDone() && !InRange())
             seeker.StartPath(rb.position, target.position, OnPathComplete);
-
     }
 
     void OnPathComplete(Path p)
@@ -56,42 +55,46 @@ public class SathanasAI : MonoBehaviour
             currentWaypoint = 0;
         }
     }
-
+    //inrange, 0.8s, charge, 
     private void Update()
     {
-        if (InShootingRange())
+        if(InRange())
         {
-            if (timeLeft < 0f)
+            if(timeLeft < 0f)
             {
-                canFire = true;
-                timeLeft = rateOfFire;
+                canCharge = true;
+                timeLeft = waitTime;
             }
             else
             {
                 timeLeft -= Time.deltaTime;
             }
 
-            if (canFire)
+            if(canCharge)
             {
-                Fire();
-                canFire = false;
+                Charge();
+                canCharge = false;
             }
+
+        }
+        else
+        {
+            timeLeft = waitTime;
         }
     }
 
-    // Update is called once per frame
     void FixedUpdate()
     {
-
-        if(path == null)
+        if (path == null)
             return;
-        
-        if(currentWaypoint >= path.vectorPath.Count || InShootingRange())
+
+        if (currentWaypoint >= path.vectorPath.Count || InRange())
         {
             reachedEndOfPath = true;
             return;
         }
-        else {
+        else
+        {
             reachedEndOfPath = false;
         }
 
@@ -104,34 +107,30 @@ public class SathanasAI : MonoBehaviour
 
         float distance = Vector2.Distance(rb.position, path.vectorPath[currentWaypoint]);
 
-        if(distance < nextWaypointDistance)
+        if (distance < nextWaypointDistance)
         {
             ++currentWaypoint;
         }
     }
 
-    bool InShootingRange()
+    bool InRange()
     {
         if (Vector2.Distance(rb.position, target.position) <= detectionRange)
         {
             return true;
         }
         else return false;
+
+
+
     }
-    
-    void Fire()
+    void Charge()
     {
         Vector2 direction = ((Vector2)target.position - rb.position).normalized;
+        Vector2 chargeForce = direction * chargeSpeed;
 
-        aim.transform.localPosition = new Vector3(0.2f * direction.x, 0.2f * direction.y);
+        rb.AddForce(chargeForce, ForceMode2D.Impulse);
 
-        var angle = Vector2.SignedAngle(Vector2.right, direction);
-        var targetRotation = new Vector3(0, 0, angle);
-        var lookTo = Quaternion.Euler(targetRotation);
-
-        GameObject bullet = Instantiate(bulletPrefab, aim.transform.position, lookTo * Quaternion.Euler(0, 0, 90));
-
-        Rigidbody2D bulletRb = bullet.GetComponent<Rigidbody2D>();
-        bulletRb.AddForce(direction * bulletForce, ForceMode2D.Impulse);
+        rb.velocity = Vector2.ClampMagnitude(rb.velocity, maxSpeed * chargeSpeed);
     }
 }
