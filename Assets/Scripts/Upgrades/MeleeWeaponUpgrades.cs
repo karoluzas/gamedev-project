334b0d10c4a011e409de84ce469d4d91889150eb
Ernestas Ščevinskas <ernestas20111@gmail.com>
2022-11-03 10:58:42 +0200

    Removed some classes

--- Assets/Scripts/Upgrades/MeleeWeaponUpgrades.cs
+++
@@ -1,7 +0,0 @@
-public class MeleeWeaponUpgrades
-{
-    public static float SwingSpeed = 1.0f;
-    public static float SwingSize = 1.0f;
-    public static float Cooldown = 0.5f;
-    public static float Damage = 1.0f;
-}
