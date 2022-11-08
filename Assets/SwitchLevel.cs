e471463934523f2b04d53c4b34f0d896ee370b53
karoluzas <79944650+karoluzas@users.noreply.github.com>
2022-11-08 20:31:30 +0200

    Scene transition crossfade

--- Assets/SwitchLevel.cs
+++ Assets/SwitchLevel.cs
@@ -6,13 +6,20 @@ using UnityEngine.SceneManagement;
 public class SwitchLevel : MonoBehaviour
 {
     public int sceneBuildIndex;
-
+    public Animator transition;
+    public float transitionTime;
     private void OnTriggerEnter2D(Collider2D other){
         print("Trigger Entered");
 
         if(other.tag == "Player"){
             print("Switching scene to " + sceneBuildIndex);
-            SceneManager.LoadScene(sceneBuildIndex, LoadSceneMode.Single);            
+            StartCoroutine(LoadLevel(sceneBuildIndex));
         }
+    }
+
+    IEnumerator LoadLevel(int levelIndex){
+        transition.SetTrigger("Start");
+        yield return new WaitForSeconds(1);
+        SceneManager.LoadScene(sceneBuildIndex, LoadSceneMode.Single);            
     }
 }
