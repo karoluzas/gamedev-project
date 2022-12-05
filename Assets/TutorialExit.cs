293b01d0b93e4baea9da423a78094b9a5f6ea976
Deividas Kučinskas <deividask.lt@gmail.com>
2022-12-05 20:11:14 +0200

    Full tutorial
    Only exit to main menu script missing, as not yet created.

---
+++ Assets/TutorialExit.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialExit : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            //TODO: exit to main menu
+        }
+    }
+}
