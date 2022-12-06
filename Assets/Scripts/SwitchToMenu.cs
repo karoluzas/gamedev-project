d62cac25035164d80cec361846be9d2b36e88799
Ernestas Ščevinskas <ernestas20111@gmail.com>
2022-12-06 18:57:02 +0200

    Fixes

--- Assets/Scripts/SwitchToMenu.cs
+++ Assets/Scripts/SwitchToMenu.cs
@@ -1,18 +1,22 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SwitchToMenu : MonoBehaviour
 {
     [SerializeField]
     private int mainMenuScene;
 
+    [SerializeField]
+    private bool listenToEscapeKey = false;
+
     public void LoadMenu()
     {
-        UnityEngine.SceneManagement.SceneManager.LoadScene(mainMenuScene);
+        UnityEngine.SceneManagement.SceneManager.LoadScene(mainMenuScene, LoadSceneMode.Single);
     }
 
     public void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
+        if (listenToEscapeKey && Input.GetKeyDown(KeyCode.Escape))
             LoadMenu();
     }
 }
