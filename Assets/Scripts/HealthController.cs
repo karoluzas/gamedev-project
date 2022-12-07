8131dcf64d1dc979c9305dfa3b01187f7b7c54cb
Ernestas Ščevinskas <ernestas20111@gmail.com>
2022-12-07 18:09:25 +0200

    Some edge cases coverage/corrections

--- Assets/Scripts/HealthController.cs
+++ Assets/Scripts/HealthController.cs
@@ -27,36 +27,44 @@ public class HealthController : MonoBehaviour
             health -= amount;
             if (gameObject.tag == "Player" || gameObject.tag == "Enemy_Sathanas")
             {
-                StartCoroutine(FlashRed());
+                if (flashDuration > 0f)
+                    StartCoroutine(FlashRed());
             }
             if(gameObject.tag == "Player")
             {
-                Instantiate(playerHurtSound);
+                if (playerHurtSound)
+                    Instantiate(playerHurtSound);
             }
             //Hurt Animation/Particles?
         }
         else
         {
             if(gameObject.tag == "Enemy_Sathanas"){
-                Instantiate(enemyDeathSound);
+                if (enemyDeathSound)
+                    Instantiate(enemyDeathSound);
                 inventoryController.AddDemonBlood(Random.Range(1, 5));
             }
-            if(gameObject.tag == "Player"){
-                Instantiate(playerDeathSound);
+            if(gameObject.tag == "Player")
+            {
+                if (playerDeathSound)
+                    Instantiate(playerDeathSound);
             }
             if(gameObject.tag == "Rock")
             {
-                Instantiate(rockBreakSound);
+                if (rockBreakSound)
+                    Instantiate(rockBreakSound);
                 inventoryController.AddRocks(Random.Range(1, 10), Random.Range(0, 2), Random.Range(0, 4));
             }
             if (gameObject.tag == "Demon Egg")
             {
-                Instantiate(rockBreakSound);
+                if (rockBreakSound)
+                    Instantiate(rockBreakSound);
                 inventoryController.AddBushes(Random.Range(1, 10), Random.Range(0, 2), Random.Range(0, 4));
             }
             if (gameObject.tag == "Demon Altar")
             {
-                Instantiate(rockBreakSound);
+                if (rockBreakSound)
+                    Instantiate(rockBreakSound);
                 inventoryController.AddAltairs(Random.Range(1, 10), Random.Range(0, 2), Random.Range(0, 4));
             }
             Destroy(gameObject, 0.1f);
