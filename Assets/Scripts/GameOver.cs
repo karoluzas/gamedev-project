1fa829dc588e7dc08bb9c27909c49df8274cf22e
Ernestas Ščevinskas <ernestas20111@gmail.com>
2022-12-08 04:19:22 +0200

    Fixed

---
+++ Assets/Scripts/GameOver.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour
+{
+    [SerializeField]
+    private int sceneIndex;
+
+    private void Start()
+    {
+        StartCoroutine(Timer());
+    }
+
+    private IEnumerator Timer()
+    {
+        
+        yield return new WaitForSeconds(3);
+        UnityEngine.SceneManagement.SceneManager.LoadScene(sceneIndex, LoadSceneMode.Single);
+    }
+}
