4459978ae3398fec4c2f8e7a14e7f217996e66c0
Ernestas Ščevinskas <ernestas20111@gmail.com>
2022-12-08 02:39:13 +0200

    fixes

--- Assets/Scripts/RenderOrder.cs
+++ Assets/Scripts/RenderOrder.cs
@@ -5,17 +5,27 @@ public class RenderOrder : MonoBehaviour
 {
     private GameObject player;
     private string[] tags = new[] { "Upgrade Station", "Demon Altar", "Spaceship", "Rock", "Demon Egg" };
+    private int biggerLayer;
+    private int smallerLayer;
 
     private void Start()
     {
         player = GameObject.FindWithTag("Player");
+        biggerLayer = gameObject.layer + 1;
+        smallerLayer = gameObject.layer - 1;
     }
 
     private void Update()
     {
         if (tags.Contains(gameObject.tag))
         {
-
+            Vector3 currentScale = gameObject.transform.localScale;
+            Vector3 playerScale = player.transform.localScale;
+            if (currentScale.y > playerScale.y)
+            {
+                gameObject.layer += 1;
+            }
+                
         }
     }
 }
